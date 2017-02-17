75200fe0475058bf9bd37d39e3dd703a3ca58859
smalldatabrains <smalldatabrains@gmail.com>
2017-02-17 14:36:25 +0700

    removing outsource functions

--- cnn3d.py
+++ cnn3d.py
@@ -7,50 +7,13 @@ import dicom
 import matplotlib.pyplot as plt
 
 #retrieve some data---------------------------------------------------------------------------------
-data_folder='G:\Data Science bowl 2017'
-#create list of patients----------------------------------------------------------------------------
-patients=os.listdir(data_folder)
+path='G:\Data Science bowl 2017'
 
-#useful functions-----------------------------------------------------------------------------------
-#load scan function made by Guido Zuidhof (Kaggle Kernel)-------------------------------------------
-def load_scan(path):
-    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
-    slices.sort(key = lambda x: int(x.ImagePositionPatient[2]))
-    try:
-        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
-    except:
-        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
-        
-    for s in slices:
-        s.SliceThickness = slice_thickness
-        
-    return slices
+#preparing the network------------------------------------------------------------------------------
+filter=[3,3,3]
 
-#transform to HU units------------------------------------------------------------------------------
-def get_pixels_hu(slices):
-    image = np.stack([s.pixel_array for s in slices])
-    # Convert to int16 (from sometimes int16), 
-    # should be possible as values should always be low enough (<32k)
-    image = image.astype(np.int16)
+def create_weights(shape):
+	return tf.Variable(tf.truncated_normal(shape=shape,stddev=0.1))
 
-    # Set outside-of-scan pixels to 0
-    # The intercept is usually -1024, so air is approximately 0
-    image[image == -2000] = 0
-    
-    # Convert to Hounsfield units (HU)
-    for slice_number in range(len(slices)):
-        
-        intercept = slices[slice_number].RescaleIntercept
-        slope = slices[slice_number].RescaleSlope
-        
-        if slope != 1:
-            image[slice_number] = slope * image[slice_number].astype(np.float64)
-            image[slice_number] = image[slice_number].astype(np.int16)
-            
-        image[slice_number] += np.int16(intercept)
-    
-    return np.array(image, dtype=np.int16)
-
-
-#visualize some data--------------------------------------------------------------------------------
-for patient in patients[0:1]:
+def create_bias(length):
+	return tf.Variable(tf.constant(0.1,shape=[length]))
